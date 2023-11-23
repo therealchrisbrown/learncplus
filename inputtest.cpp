7c9f067d6bc35e13d7e173eb34f55a2c6cb3f5b5
therealchrisbrown <ch.braun1@gmx.de>
2023-11-23 09:45:46 +0100

    first input test

---
+++ inputtest.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string x;
+    cout << "Enter your name: ";
+    cin >> x; //Get user's name as input
+    cout << "Your name is: " << x; //Display the input value
+}
